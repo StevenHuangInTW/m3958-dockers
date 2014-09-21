ec7bcfaefc9ae92a0704f0977ec6bb56d2d328ee
jianglibo <jianglibo@gmail.com>
2014-09-21 14:17:13 +0800

    export JAVA_HOME

--- anonymousupload/runvertx.sh
+++ anonymousupload/runvertx.sh
@@ -2,6 +2,6 @@
 
 cd /opt/runningdir
 
-JAVA_HOME=/etc/alternatives/java_sdk_openjdk
+export JAVA_HOME="/etc/alternatives/java_sdk_openjdk"
 
 /vert.x-2.1.2/bin/vertx runzip anonymousupload-0.0.5-mod.zip
