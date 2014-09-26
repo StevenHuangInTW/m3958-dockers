662638cdb2852970ecb60cacd41d06395c8e4132
jianglibo <jianglibo@gmail.com>
2014-09-26 14:50:37 +0800

    change docker run name

--- anonymousupload/rundocker.sh
+++ anonymousupload/rundocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --privileged=true -p 27017:27017 -d --name vertx -v /opt/dockerdata/anonymousupload:/opt/runningdir m3958/anonymousupload
+docker run --privileged=true -p 27017:27017 -d --name vertxanonymousupload -v /opt/dockerdata/anonymousupload:/opt/runningdir m3958/anonymousupload
 
 #--privileged=true
--- vertx-combo/rundocker.sh
+++ vertx-combo/rundocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --privileged=true -p 8093:8093 -d --name vertx -v /opt/dockerdata/vertxcombo:/opt/runningdir m3958/vertxcombo
+docker run --privileged=true -p 8093:8093 -d --name vertxcombo -v /opt/dockerdata/vertxcombo:/opt/runningdir m3958/vertxcombo
 
 #--privileged=true
