0adf3fac26c2c3e35aa861b80ef83af60b12fe8e
jianglibo <jianglibo@gmail.com>
2014-09-26 17:20:41 +0800

    chang rundocker port number

--- anonymousupload/rundocker.sh
+++ anonymousupload/rundocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --privileged=true -p 27017:27017 -d --name vertxanonymousupload -v /opt/dockerdata/anonymousupload:/opt/runningdir m3958/anonymousupload
+docker run --privileged=true -p 8999:80 -d --name vertxanonymousupload -v /opt/dockerdata/anonymousupload:/opt/runningdir m3958/anonymousupload
 
 #--privileged=true
