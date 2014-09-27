c6fb3d8f4a7d10150eaf6b88ea9db097dc408207
jianglibo <jianglibo@gmail.com>
2014-09-27 16:42:03 +0800

    solr use new config

--- solr/runinhost_once.sh
+++ solr/runinhost_once.sh
@@ -5,6 +5,7 @@ tdir=/opt/dockerdata/solr
 if [ ! -d $tdir ]; then
   mkdir -p $tdir
 
+  cp -R solr $tdir
   mkdir -p "$tdir/log/mongodb"
   mkdir -p "$tdir/mongodb"
 
@@ -20,9 +21,3 @@ if [ ! -d $tdir ]; then
 else
   echo "$tdir already exist."
 fi
-
-#mannuly execute following command, tar -zxvf, then copy to tdir.
-
-#curl -o http://mirrors.cnnic.cn/apache/lucene/solr/4.10.0/solr-4.10.0.tgz
-#curl -o http://central.maven.org/maven2/org/apache/lucene/lucene-analyzers-common/4.10.0/lucene-analyzers-common-4.10.0.jar
-# cp solrschema4100.xml
--- solr/runsolr.sh
+++ solr/runsolr.sh
@@ -2,4 +2,4 @@
 
 cd /solr-4.10.0/example
 
-/usr/bin/java -Dsolr.solr.home=multicore -jar start.jar
+/usr/bin/java -jar start.jar
