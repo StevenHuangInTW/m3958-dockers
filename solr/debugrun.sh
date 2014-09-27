fd47e36b536817e23cf3a89661f8c987d0f4a6fc
jianglibo <jianglibo@gmail.com>
2014-09-27 16:44:33 +0800

    fix solr

--- solr/debugrun.sh
+++ solr/debugrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -it -v /opt/dockerdata/solr:/opt/runningdir -v /opt/dockerdata/solr/multicore:/solr-4.10.0/example/multicore m3958/solr /bin/bash
+docker run --rm -it -v /opt/dockerdata/solr:/opt/runningdir -v /opt/dockerdata/solr/solr:/solr-4.10.0/example/solr m3958/solr /bin/bash
 
 #--privileged=true
--- solr/rundocker.sh
+++ solr/rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8983:8983 -d --name solr -v /opt/dockerdata/solr:/opt/runningdir -v /opt/dockerdata/solr/multicore:/solr-4.10.0/example/multicore m3958/solr
+docker run -p 8983:8983 -d --name solr -v /opt/dockerdata/solr:/opt/runningdir -v /opt/dockerdata/solr/solr:/solr-4.10.0/example/solr m3958/solr
