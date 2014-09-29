75010ede1410ecdaaa2477a49e5475b8d6626bde
jianglibo <jianglibo@gmail.com>
2014-09-29 16:39:17 +0800

    not use supervisord to start rabbitmq

--- rabbitmq/runrabbitmq.sh
+++ rabbitmq/runrabbitmq.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+trap "{ echo Stopping rabbitmq; rabbitmqctl stop; exit 0; }" EXIT
+
 duser=$(stat -c %U /opt/runningdir)
 
 if [ ! $duser = "rabbitmq" ]
@@ -7,4 +9,6 @@ then
   chown -R rabbitmq:rabbitmq /opt/runningdir
 fi
 
+echo "start rabbitmq..."
 exec /usr/sbin/rabbitmq-server
+echo "start rabbitmq done"
