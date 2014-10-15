e1c97c4f5286033e8c83d79f18bad4eacd739ecf
jianglibo <jianglibo@gmail.com>
2014-10-15 14:48:01 +0800

    m

--- clojure/dockerbuild.sh
+++ clojure/dockerbuild.sh
@@ -4,8 +4,9 @@ curdir="$(dirname ${BASH_SOURCE[0]})"
 
 pushd $curdir >/dev/null
 
-IMG_NAME="m3958/base:201401"
+IMG_NAME="m3958/clojure-dev"
 
 docker build -t ${IMG_NAME} .
 
 popd >/dev/null
+
--- perl/dbisqlite.pl
+++ perl/dbisqlite.pl
@@ -1,47 +1,80 @@
 #!/usr/bin/env perl
 
-use feature ':5.10';
+use feature ':5.20.1';
 use DBI;
+
+use constant DSN => "dbi:SQLite:dockers.db";
+
+=for comment
+available command:
+list: app,images
+add: app, images
+start appname
+stop appname
+debug containername
+
+schemas:
+image: id,name,tag,dsc,itype
+  itype: service, vol
+apptpl: id,name,dec
+containertpl: id, imgId,cport,
+app: id,name,dsc
+container: id,apptplId,imgId,name,ctype,volumesFrom,cid,cport,hport,linkTo
+  ctype: service, service-vol, web-vol
+
+=cut
+
 my @ary = DBI->available_drivers();
 print join("\n", @ary), "\n";
 
-
-my $dbh = DBI->connect ( "dbi:SQLite:dbname=test.db", "", "", {
+my $dbh = DBI->connect ( DSN, "", "", {
     RaiseError => 1,
     AutoCommit => 0
   }) or die $DBI::errstr;
 
-my $sth = $dbh->prepare("SELECT SQLITE_VERSION()");
-
-$sth->execute();
-
-my $ver = $sth->fetch();
-say @$ver;
-
-
-$dbh->do("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT)") unless (grep(qr/^Friends$/, $dbh->tables()));
-$dbh->do("INSERT INTO Friends(Name) VALUES ('Tom')");
-$dbh->do("INSERT INTO Friends(Name) VALUES ('Rebecca')");
-$dbh->do("INSERT INTO Friends(Name) VALUES ('Jim')");
-$dbh->do("INSERT INTO Friends(Name) VALUES ('Robert')");
-$dbh->do("INSERT INTO Friends(Name) VALUES ('Julian')");
+$dbh->do("CREATE TABLE image(Id INTEGER PRIMARY KEY, Name TEXT)") unless (grep(qr/^Friends$/, $dbh->tables()));
 
 $dbh->commit;
-    
-my $id = $dbh->last_insert_id("", "", "Friends", "");
-print "The last Id of the inserted row is $id\n";
 
-
-$sth = $dbh->prepare("SELECT * FROM Friends LIMIT 5");
-$sth->execute();
-
-my $row;
-while ($row = $sth->fetchrow_arrayref()) {
-    print "@$row[0] @$row[1] @$row[2]\n";
+ENTRYPOINT: while (<STDIN>) {
+  say $_;
+  chomp;
+  if (/^exit$/ || /^quit$/) {last ENTRYPOINT}
+  if (/^querydocker$/) {
+    my @allcontainers = qx/docker ps -a/;
+    print join("\n", @allcontainers), "\n";
+    say "done!";
+  }
+  if (/^rundocker$/) {
+    my @cmd = ("docker", "run", "-it", "m3958/base:201401", "/bin/bash");
+    system(@cmd);
+    say "done!";
+  }
 }
 
-$sth->finish();
 $dbh->disconnect();
+
+#$sth->finish();
+#$dbh->do("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT)") unless (grep(qr/^Friends$/, $dbh->tables()));
+#$dbh->do("INSERT INTO Friends(Name) VALUES ('Tom')");
+#$dbh->do("INSERT INTO Friends(Name) VALUES ('Rebecca')");
+#$dbh->do("INSERT INTO Friends(Name) VALUES ('Jim')");
+#$dbh->do("INSERT INTO Friends(Name) VALUES ('Robert')");
+#$dbh->do("INSERT INTO Friends(Name) VALUES ('Julian')");
+#
+#    
+#my $id = $dbh->last_insert_id("", "", "Friends", "");
+#print "The last Id of the inserted row is $id\n";
+#
+#
+#$sth = $dbh->prepare("SELECT * FROM Friends LIMIT 5");
+#$sth->execute();
+#
+#my $row;
+#while ($row = $sth->fetchrow_arrayref()) {
+#    print "@$row[0] @$row[1] @$row[2]\n";
+#}
+
 
 ## for select
 #$sth = $dbh->prepare("SELECT foo, bar FROM table WHERE baz=?";
@@ -59,3 +92,12 @@ $dbh->disconnect();
 #}
 ## for non repeated non-select
 #$rows_affected = $dbh->do("UPDATE your_table SET foo = foo + 1");
+#
+#my $sth = $dbh->prepare("SELECT SQLITE_VERSION()");
+#
+#$sth->execute();
+#
+#my $ver = $sth->fetch();
+#say @$ver;
+#my @ary = DBI->available_drivers();
+#print join("\n", @ary), "\n";
